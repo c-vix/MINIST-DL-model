0794b027d0633bb814ca03efb7c5abb73a86f9c0
chathush-vish <chathushuni@gmail.com>
2026-09-20 14:00:34 +0530

    Trained the model

--- train.py
+++ train.py
@@ -3,6 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Load MNIST dataset
+# --------------------
+
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 
 # Dataset information
@@ -21,5 +23,51 @@ import matplotlib.pyplot as plt
 
 # Normalize the data
 # ------------------
+
 x_train = x_train / 255.0
 x_test = x_test / 255.0
+
+# Building the model
+# ------------------
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(10, activation='softmax') 
+])
+
+# Compile the model
+# -----------------
+
+model.compile(
+    optimizer='adam',
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy']
+)
+
+# Train the model
+# -----------------
+
+model.fit(
+    x_train,
+    y_train,
+    epochs=5,
+    batch_size=32,
+    validation_split=0.2
+)
+
+# 4. Compile model
+# --------------------------------
+
+model.compile(
+    optimizer="adam",
+    loss="sparse_categorical_crossentropy",
+    metrics=["accuracy"]
+)
+
+# 7. Save model
+# --------------------------------
+
+model.save("mnist_model.keras")
+
+print("Model saved!")
